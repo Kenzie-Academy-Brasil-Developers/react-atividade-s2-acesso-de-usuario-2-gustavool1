5baf8d856a5a15dfba85cecfe97d8fe29179174d
gustavo <go8895806@gmail.com>
2021-09-14 12:51:58 -0300

    .

--- atividade04/src/components/MembersList/index.jsx
+++ atividade04/src/components/MembersList/index.jsx
@@ -1,26 +1,16 @@
 import { members } from "../../helper.js"
 import './style.css'
-import { useHistory, Link} from "react-router-dom"
+import { Link} from "react-router-dom"
 const MembersList = () =>{
-    const history = useHistory()
-    const handleClick = (id) =>{
-        const filtered = members.find((member)=>member.id === id)
-        if(filtered.type === 'pj'){
-            history.push('/company/'+id)
-        }else{
-            history.push('/costumer/'+id)
-        }
-        
-    }
     return(
         <div className='members-container'>
             <h1>Membros</h1>
             <ul>
                 {members.map((member)=>(            
                     member.type === 'pj'? (
-                        <Link  to={'/company/'+member.id}>{member.name}</Link>
+                        <Link  to={'/company/'+member.id} key={member.id}>{member.name}</Link>
                     ):(
-                        <Link  to={'/customer/'+member.id}>{member.name}</Link>
+                        <Link  to={'/customer/'+member.id} key={member.id}>{member.name}</Link>
                     )
                 ))}            
             </ul>
